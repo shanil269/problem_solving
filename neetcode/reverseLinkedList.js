c0d894853f6d163afc36b96cdf6d0b5947ae0b5b
Md Muhaimin Hossain <shanilhossain2609@gmail.com>
2024-05-19 19:16:35 +0900

    reverse linked list with recursion

--- neetcode/reverseLinkedList.js
+++ neetcode/reverseLinkedList.js
@@ -45,3 +45,21 @@ var reverseList = function (head) {
 };
 
 console.log(reverseList(head));
+
+// solution with Recursion
+
+var reverseList = function (head) {
+    const isBaseCase = !head?.next;
+    if (isBaseCase) return head;
+
+    return dfs(head);                   /* Time O(N) | Space O(N) */
+}
+
+const dfs = (curr) => {
+    const prev = reverseList(curr.next);/* Time O(N) | Space O(N) */
+
+    curr.next.next = curr;
+    curr.next = null;
+
+    return prev;
+}
