aea187a9245e64042234f23e4e89700a064f4993
Md Muhaimin Hossain <shanilhossain2609@gmail.com>
2024-05-23 20:19:55 +0900

    merge sort

--- neetcode/sortanArray.js
+++ neetcode/sortanArray.js
@@ -28,17 +28,115 @@ Constraints:
  * @return {number[]}
  */
 let nums = [5, 1, 1, 2, 0, 0];
+
+// insertion sort
+// var sortArray1 = function (nums) {
+//     for (let i = 0; i < nums.length; i++) {
+//         let j = i + 1;
+//         while (j >= 0 && nums[j] < nums[j - 1]) {
+//             let temp = nums[j - 1];
+//             nums[j - 1] = nums[j];
+//             nums[j] = temp;
+//             j--;
+//         }
+//     }
+//     return nums;
+// };
+
+// merge sort
 var sortArray = function (nums) {
-    for (let i = 0; i < nums.length; i++) {
-        let j = i + 1;
-        while (j >= 0 && nums[j] < nums[j - 1]) {
-            let temp = nums[j - 1];
-            nums[j - 1] = nums[j];
-            nums[j] = temp;
-            j--;
-        }
-    }
-    return nums;
+    return mergeSort(nums, 0, nums.length - 1);
 };
+
+function mergeSort(arr, left, right) {
+    if (left === right) {
+        return arr;
+    }
+
+    let mid = Math.floor((left + right) / 2);
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+
+    return merge(arr, left, mid, right);
+};
+
+function merge(arr, left, mid, right) {
+    let arr1 = arr.slice(left, mid + 1);
+    let arr2 = arr.slice(mid + 1, right + 1);
+
+    let p1 = 0;
+    let p2 = 0;
+    let gp = left;
+
+    while (p1 < arr1.length && p2 < arr2.length) {
+        if (arr1[p1] < arr2[p2]) {
+            arr[gp] = arr1[p1];
+            p1++;
+        } else {
+            arr[gp] = arr2[p2];
+            p2++;
+        }
+        gp++
+    }
+
+    while (p1 < arr1.length) {
+        arr[gp] = arr1[p1];
+        p1++;
+        gp++;
+    }
+    while (p2 < arr2.length) {
+        arr[gp] = arr2[p2];
+        p2++;
+        gp++;
+    }
+
+    return arr;
+}
+// const sortArray = function (nums) {
+//     return mergeSort(0, nums.length - 1, nums);
+// };
+
+// const mergeSort = (left, right, nums) => {
+
+//     if (left === right) return nums;
+
+//     const mid = Math.floor((left + right) / 2);
+//     mergeSort(left, mid, nums);
+//     mergeSort(mid + 1, right, nums);
+//     return merge(left, right, mid, nums);
+// }
+
+// const merge = (left, right, mid, nums) => {
+//     const arr1 = nums.slice(left, mid + 1);
+//     const arr2 = nums.slice(mid + 1, right + 1);
+
+//     let p1 = 0;
+//     let p2 = 0;
+//     let gp = left;
+
+//     while (p1 < arr1.length && p2 < arr2.length) {
+//         if (arr1[p1] < arr2[p2]) {
+//             nums[gp] = arr1[p1];
+//             p1++;
+//         } else {
+//             nums[gp] = arr2[p2];
+//             p2++;
+//         }
+//         gp++;
+//     }
+
+//     while (p1 < arr1.length) {
+//         nums[gp] = arr1[p1];
+//         p1++;
+//         gp++;
+//     }
+
+//     while (p2 < arr2.length) {
+//         nums[gp] = arr2[p2];
+//         p2++;
+//         gp++;
+//     }
+//     return nums;
+// }
 
 console.log(sortArray(nums));
